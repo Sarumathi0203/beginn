fbf5a4b4e57e8882ef2a9933d882be0842c15cd1
saruhirthick <43338229+saruhirthick@users.noreply.github.com>
2018-09-18 14:38:53 +0530

    Create prgrm1.c

---
+++ prgrm1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n=5;
+clrscr();
+if(n>0)
+{
+printf("positive\n");
+}
+else if(n<0)
+{
+printf("negative\n");
+}
+else if(n==0)
+{
+printf("zero\n");
+}
+getch();
+}
